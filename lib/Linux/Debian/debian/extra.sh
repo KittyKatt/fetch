0187774cbcba062b3385cba152b854b887ae122c
KittyKatt <kittykatt@kittykatt.co>
2021-03-04 14:18:51 -0600

    Fixes for debian/extra.sh

--- lib/Linux/Debian/debian/extra.sh
+++ lib/Linux/Debian/debian/extra.sh
@@ -1,19 +1,19 @@
 if [ -f /etc/siduction-version ]; then
-    distro="Siduction"
+    my_distro="Siduction"
     distro_release="(Debian Sid)"
     distro_codename=
 elif [ -f /usr/bin/pveversion ]; then
-    distro="Proxmox VE"
+    my_distro="Proxmox VE"
     distro_codename=
     distro_release="$(/usr/bin/pveversion | grep -oP 'pve-manager\/\K\d+\.\d+')"
 elif [ -f /etc/os-release ]; then
     if grep -q -i 'Raspbian' /etc/os-release ; then
-        distro="Raspbian"
+        my_distro="Raspbian"
         distro_release=$(awk -F'=' '/^PRETTY_NAME=/ {print $2}' /etc/os-release)
     elif grep -q -i 'BlankOn' /etc/os-release ; then
-        distro='BlankOn'
+        my_distro='BlankOn'
         distro_release=$(awk -F'=' '/^PRETTY_NAME=/ {print $2}' /etc/os-release)
     else
-        distro="Debian"
+        my_distro="Debian"
     fi
 fi
