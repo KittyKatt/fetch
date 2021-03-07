b645157b3e54c8a3aad6f77bc18778e35c3463e4
KittyKatt <kittykatt@kittykatt.co>
2021-03-06 22:50:20 -0600

    Let's see if the first few tests work.

--- tests/ubuntu.sh
+++ tests/ubuntu.sh
@@ -1,3 +1,11 @@
-IFS=$'\n' read -r -a _output <<< "$(bash fetch.sh --config sample.config.conf -v)"
+#!/usr/bin/env bash
+n=0
+IFS=$'\n'
+while read -r line; do
+    _output[${n}]="${line}"
+    ((n++))
+done <<< "$(bash fetch.sh --config sample.config.conf -v)"
 
-[[ ! ${_output[0]} =~ ^(.*)'Finding kernel...found as'(.*)'Linux '[[:digit:]]+'.'[[:digit:]]+'.'[[:digit:]]+  ]] && echo "Failed on kernel."; exit 1
+[[ ! ${_output[0]} =~ ^(.*)'Finding kernel...found as'(.*)'Linux '[[:digit:]]+'.'[[:digit:]]+'.'[[:digit:]]+'-'[[:digit:]]+'-azure' ]] && echo "Failed on kernel."; exit 1
+[[ ! ${_output[1]} =~ ^(.*)'Finding OS...found as'(.*)'Linux'(.*)'.' ]] && echo "Failed on OS."; exit 1
+[[ ! ${_output[2]} =~ ^(.*)'Finding user info...found as'(.*)'runner@'[^[:space:]]+'.' ]] && echo "Failed on OS."; exit 1
