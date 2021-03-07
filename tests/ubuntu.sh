726f49e3f8077f2d8dfbfe29e237d8ba2e81f8a3
KittyKatt <kittykatt@kittykatt.co>
2021-03-06 23:11:59 -0600

    More test logic

--- tests/ubuntu.sh
+++ tests/ubuntu.sh
@@ -7,22 +7,29 @@ while read -r line; do
 done <<< "$(bash fetch.sh --config sample.config.conf -v)"
 
 if [[ ! ${_output[0]} =~ ^(.*)'Finding kernel...found as'(.*)'Linux '[[:digit:]]+'.'[[:digit:]]+'.'[[:digit:]]+'-'[[:digit:]]+'-azure' ]]; then
-    echo "Failed on kernel."
-    exit 1
+    echo "! Failed on kernel."
+    echo "  failed line: ${_output[0]}"
+    echo "  \$(uname -srm): $(uname -srm)"
+    _output[0]+="[FAILED]"
 else
     echo "Kernel succeeded."
 fi
 
 if [[ ! ${_output[1]} =~ ^(.*)'Finding OS...found as'(.*)'Linux'(.*)'.' ]]; then
-    echo "Failed on OS."
-    exit 1
+    echo "! Failed on OS."
+    _output[1]+="[FAILED]"
 else
-    echo "OS succeeded."
+    echo "> OS succeeded."
 fi
 
 if [[ ! ${_output[2]} =~ ^(.*)'Finding user info...found as'(.*)'runner@'[^[:space:]]+'.' ]]; then
-    echo "Failed on user info."
-    exit 1
+    echo "! Failed on user info."
+    echo "  \${USER}: ${USER}"
+    echo "  \${HOSTNAME}: ${HOSTNAME}"
+    echo "  \$(hostname): $(hostname)"
+    _output[2]+="[FAILED]"
 else
     echo "User info succeeded."
 fi
+
+[[ ${_output[*]} =~ \[FAILED\] ]] && echo "[ TESTS FAILED ]"; exit 1
