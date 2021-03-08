6d496dd21e4ffefe94ea1348bab049c548b855b8
KittyKatt <kittykatt@kittykatt.co>
2021-03-08 16:42:23 +0000

    Some fixes to tests

--- tests/ubuntu.sh
+++ tests/ubuntu.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 f=0
-trap 'f=1' ERR
+trap 'f=$((f+1))' ERR
 
 source ./lib/Linux/Ubuntu/ubuntu/ascii.sh
 
 reset=$'\e[0m'
 successOut () {
     success=$'\033[0m\033[32m'
-    printf '>> %s\n' "${success}${1}${reset}"
+    printf '>> %b\n' "${success}${1}${reset}"
 }
 errorOut () {
     error=$'\033[0m\033[1;31m'
-    printf '%s\n' "${error}${1}${reset}"
+    printf '%b\n' "${error}${1}${reset}"
 }
 
 n=0
@@ -25,15 +25,15 @@ done <<< "$(bash fetch.sh --config sample.config.conf -v)"
 if [[ ! ${_output[0]} =~ ^(.*)'Finding kernel...found as'(.*)'Linux '[[:digit:]]+'.'[[:digit:]]+'.'[[:digit:]]+'-'[[:digit:]]+'-azure' ]]; then
     errorOut "!! Failed on kernel."
     errorOut "\tfailed line: ${_output[0]}"
-    errorOut '\t%s\n' "\t\$(uname -srm): $(uname -srm)"
-    f=1
+    errorOut "\t\$(uname -srm): $(uname -srm)"
+    ((f++))
 else
     successOut "Kernel succeeded."
 fi
 
 if [[ ! ${_output[1]} =~ ^(.*)'Finding OS...found as'(.*)'Linux'(.*)'.' ]]; then
     errorOut "!! Failed on OS."
-    f=1
+    ((f++))
 else
     successOut "OS succeeded."
 fi
@@ -43,7 +43,7 @@ if [[ ! ${_output[2]} =~ ^(.*)'Finding user info...found as'(.*)'runner@'[^[:spa
     errorOut "\t\${USER}: ${USER}"
     errorOut "\t\${HOSTNAME}: ${HOSTNAME}"
     errorOut "\t\$(hostname): $(hostname)"
-    f=1
+    ((f++))
 else
     successOut "User info succeeded."
 fi
@@ -51,6 +51,13 @@ fi
 # count number of verbosity lines
 numlines=$(awk -F'\n' '/:: Finding/ {print NF}' <<< "${_output[@]}")
 printf '%s\n' "${numlines}"
+
+if ((f == 0)); then
+    exit 0
+else
+    errorOut "failures: ${f}"
+    exit 1
+fi
 
 #for (( i = 0; i <= n; i++)); do
 #    printf '%s\n' "${i}"
