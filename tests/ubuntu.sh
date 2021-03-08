770cbd7d037105e46f1013c0980ad4ea6a27d607
KittyKatt <kittykatt@kittykatt.co>
2021-03-08 10:25:24 -0600

    Rewriting some bits to see if we can just loop over the verbosity lines for comparison

--- tests/ubuntu.sh
+++ tests/ubuntu.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 f=0
-trap 'f=$((f+1))' ERR
+trap 'f=1' ERR
 
-success='\033[0m\033[32m'
-error='\033[0m\033[1;31m'
-reset='\e[0m'
+source ../lib/Linux/Ubuntu/ubuntu/ascii.sh
+
+reset=$'\e[0m'
+successOut () {
+    success=$'\033[0m\033[32m'
+    printf '>> %s\n' "${success}${1}${reset}"
+}
+errorOut () {
+    error=$'\033[0m\033[1;31m'
+    printf '%s\n' "${error}${1}${reset}"
+}
 
 n=0
 IFS=$'\n'
@@ -15,31 +23,35 @@ while read -r line; do
 done <<< "$(bash fetch.sh --config sample.config.conf -v)"
 
 if [[ ! ${_output[0]} =~ ^(.*)'Finding kernel...found as'(.*)'Linux '[[:digit:]]+'.'[[:digit:]]+'.'[[:digit:]]+'-'[[:digit:]]+'-azure' ]]; then
-    printf '%s\n' "${error}! Failed on kernel.${reset}"
-    printf '\t%s\n' "${error}failed line: ${_output[0]}${reset}"
-    printf '\t%s\n' "${error}\$(uname -srm): $(uname -srm)${reset}"
-    _output[0]+="[FAILED]"
+    errorOut "!! Failed on kernel."
+    errorOut "\tfailed line: ${_output[0]}"
+    errorOut '\t%s\n' "\t\$(uname -srm): $(uname -srm)"
+    f=1
 else
-    printf '%s\n' "${success}Kernel succeeded.${reset}"
+    succcessOut "Kernel succeeded."
 fi
 
 if [[ ! ${_output[1]} =~ ^(.*)'Finding OS...found as'(.*)'Linux'(.*)'.' ]]; then
-    printf '%s\n' "${error}! Failed on OS.${reset}"
-    _output[1]+="[FAILED]"
+    errorOut "!! Failed on OS."
+    f=1
 else
-    printf '%s\n' "${success}OS succeeded.${reset}"
+    succcessOut "OS succeeded."
 fi
 
 if [[ ! ${_output[2]} =~ ^(.*)'Finding user info...found as'(.*)'runner@'[^[:space:]]+'.' ]]; then
-    printf '%s\n' "${error}! Failed on user info.${reset}"
-    printf '\t%s\n' "${error}\${USER}: ${USER}${reset}"
-    printf '\t%s\n' "${error}\${HOSTNAME}: ${HOSTNAME}${reset}"
-    printf '\t%s\n' "${error}\$(hostname): $(hostname)${reset}"
-    _output[2]+="[FAILED]"
+    errorOut "!! Failed on user info."
+    errorOut "\t\${USER}: ${USER}"
+    errorOut "\t\${HOSTNAME}: ${HOSTNAME}"
+    errorOut "\t\$(hostname): $(hostname)"
+    f=1
 else
-    printf '%s\n' "${success}User info succeeded.${reset}"
+    succcessOut "User info succeeded."
 fi
 
-for (( i = 0; i <= n; i++)); do
-    printf '%s\n' "${i}"
-done
+# count number of verbosity lines
+numlines=$(awk -F=$'\n' '/^:: Finding/ {print NF}' <<< "${_output[@]}")
+printf '%s\n' "${numlines}"
+
+#for (( i = 0; i <= n; i++)); do
+#    printf '%s\n' "${i}"
+#done
