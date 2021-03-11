032a5f2fe8cfaf34120f9d72bad2d61e89ffe8e2
KittyKatt <kittykatt@kittykatt.co>
2021-03-11 15:19:45 -0600

    More ASCII additions

---
+++ ascii/black panther os.sh
@@ -0,0 +1,30 @@
+# logo width: 38
+# number of colors: 4
+c1=$(getColor 'yellow') # Light Yellow
+c2=$(getColor 'white') # Bold Red
+c3=$(getColor 'light red') # Light Red
+c4=$(getColor 'dark grey')
+startline="0"
+read -rd '' asciiLogo <<'EOF'
+${c4}                oxoo
+${c4}           ooooooxxxxxxxx
+${c4}      oooooxxxxxxxxxx${c3}O${c1}o.${c4}xx
+${c4}    oo# ###xxxxxxxxxxx###xxx
+${c4}  oo .oooooxxxxxxxxx##   #oxx
+${c4} o  ##xxxxxxxxx###x##   .o###
+${c4}  .oxxxxxxxx###   ox  .
+${c4} ooxxxx#xxxxxx     o##
+${c4}.oxx# #oxxxxx#
+${c4}ox#  ooxxxxxx#                  o
+${c4}x#  ooxxxxxxxx           ox     ox
+${c4}x# .oxxxxxxxxxxx        o#     oox
+${c4}#  oxxxxx##xxxxxxooooooo#      o#
+${c4}  .oxxxxxooxxxxxx######       ox#
+${c4}  oxxxxxo oxxxxxxxx         oox##
+${c4}  oxxxxxx  oxxxxxxxxxo   oooox##
+${c4}   o#xxxxx  oxxxxxxxxxxxxxxxx##
+${c4}    ##xxxxx  o#xxxxxxxxxxxxx##
+${c4}      ##xxxx   o#xxxxxxxxx##
+${c4}         ###xo.  o##xxx###
+${c4}
+EOF
---
+++ ascii/centos.sh
@@ -0,0 +1,28 @@
+# logo width: 40
+# number of colors: 4
+c1=$(getColor 'yellow')
+c2=$(getColor 'light green')
+c3=$(getColor 'light blue')
+c4=$(getColor 'light purple')
+startline="0"
+read -rd '' asciiLogo <<'EOF'
+${c1}                   ..
+${c1}                 .PLTJ.
+${c1}                <><><><>
+${c2}       KKSSV' 4KKK ${c1}LJ${c4} KKKL.'VSSKK
+${c2}       KKV' 4KKKKK ${c1}LJ${c4} KKKKAL 'VKK
+${c2}       V' ' 'VKKKK ${c1}LJ${c4} KKKKV' ' 'V
+${c2}       .4MA.' 'VKK ${c1}LJ${c4} KKV' '.4Mb.
+${c4}     . ${c2}KKKKKA.' 'V ${c1}LJ${c4} V' '.4KKKKK ${c3}.
+${c4}   .4D ${c2}KKKKKKKA.'' ${c1}LJ${c4} ''.4KKKKKKK ${c3}FA.
+${c4}  <QDD ++++++++++++  ${c3}++++++++++++ GFD>
+${c4}   'VD ${c3}KKKKKKKK'.. ${c2}LJ ${c1}..'KKKKKKKK ${c3}FV
+${c4}     ' ${c3}VKKKKK'. .4 ${c2}LJ ${c1}K. .'KKKKKV ${c3}'
+${c3}        'VK'. .4KK ${c2}LJ ${c1}KKA. .'KV'
+${c3}       A. . .4KKKK ${c2}LJ ${c1}KKKKA. . .4
+${c3}       KKA. 'KKKKK ${c2}LJ ${c1}KKKKK' .4KK
+${c3}       KKSSA. VKKK ${c2}LJ ${c1}KKKV .4SSKK
+${c2}                <><><><>
+${c2}                 'MKKM'
+${c2}                   ''
+EOF
