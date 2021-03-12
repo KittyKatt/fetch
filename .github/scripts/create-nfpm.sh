feaae69bd45a1e3620709179f165bc3231a712fa
KittyKatt <kittykatt@kittykatt.co>
2021-03-12 15:32:06 -0600

    Fixing script to work with new action

--- .github/scripts/create-nfpm.sh
+++ .github/scripts/create-nfpm.sh
@@ -6,37 +6,24 @@ _version="${4}"
 
 read -rd '' yaml <<EOF
 name: ${_name}
-arch: all
 version: ${_version}
 maintainer: ${_maintainer}
 description: ${_desc}
+depends:
+  - bash (>=4.0)
+  - awk
 
-contents:
-  - src: lib
-    dst: /usr/share/fetch/lib
-  - src:
-    dst: /usr/share/fetch/ascii
-  - src: fetch.sh
-    dst: /usr/local/bin/fetch
-    file_info:
-      mode: 0755
-  - src: sample.config.yaml
-    dst: /usr/share/fetch/config.yaml
-    file_info:
-      mode: 0666
-    type: config
-  - src: LICENSE
-    dst: /usr/share/fetch/LICENSE
-
-overrides:
-  deb:
-    depends:
-      - bash (>= 4.0)
-      - awk
-  rpm:
-    depends:
-      - bash (>= 4.0)
-      - awk
+files:
+  "/usr/local/bin/fetch":
+    file: fetch.sh
+    mode: "0755"
+    user: "root"
+  "/usr/share/fetch/ascii":
+    file: ascii
+  "/usr/share/fetch/lib":
+    file: lib
+  "/usr/share/fetch/config.yaml":
+    file: sample.config.yaml
 EOF
 
 printf '%s\n' "${yaml}" > configuration.yaml
