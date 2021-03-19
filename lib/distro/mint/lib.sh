f1519dd4253612c1dba70e2575443276e83cbc7f
KittyKatt <kittykatt@kittykatt.co>
2021-03-19 13:18:50 -0500

    Hopefully the last round of shfmt fixes

--- lib/distro/mint/lib.sh
+++ lib/distro/mint/lib.sh
@@ -4,7 +4,7 @@ if [ "${distro_codename}" == "debian" ]; then
     my_distro="LMDE"
     distro_codename="n/a"
     distro_release="n/a"
-#adding support for LMDE 3	
+#adding support for LMDE 3
 elif [[ $(lsb_release -sd) =~ "LMDE" ]]; then
-    my_distro="LMDE"	
+    my_distro="LMDE"
 fi
