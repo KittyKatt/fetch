d72fd4a5b6172bf3307bcf466ba54175d80852b8
KittyKatt <kittykatt@kittykatt.co>
2021-03-20 14:09:57 -0500

    refactor(Configuration): Reworked configuration checks so that now it copies default config to user location if none found

--- lib/config.sh
+++ lib/config.sh
@@ -16,6 +16,23 @@ fetchConfig() {
             fi
         done < "${1}"
     else
-        errorOut "No configuration file specified"
+        errorOut "No user configuration found, looking for sample config..."
+        if [ -f "${FETCH_DATA_DIR:-/usr/share/fetch}/sample.config.conf" ]; then
+            errorOut "Found sample configuration at ${FETCH_DATA_DIR:-/usr/share/fetch}/sample.config.conf. Copying to default path..."
+            cp "${FETCH_DATA_DIR:-/usr/share/fetch}/sample.config.conf" "${FETCH_DATA_USER_DIR}/${FETCH_CONFIG_FILENAME}"
+            if [ -f "${FETCH_DATA_USER_DIR}/${FETCH_CONFIG_FILENAME}" ]; then
+                errorOut "Copied configuration file to ${FETCH_DATA_USER_DIR}/${FETCH_CONFIG_FILENAME}"
+                errorOut "Please run fetch again."
+                exit 0
+            else
+                errorOut "Failed to copy configuration. Please open a bug report at ${FETCH_SRC_LOCATION}"
+                exit 1
+            fi
+        else
+            errorOut "Could not find sample configuration file...did you install fetch correctly?" 
+            errorOut" Please consult ${FETCH_SRC_LOCATION} or your package manager for installation instructions."
+            exit 1
+        fi
+
     fi
 }
