3c7db2c101d2cc607e9727e2364ec8486dceb3ac
KittyKatt <kittykatt@kittykatt.co>
2021-03-20 14:12:51 -0500

    Updating tests for new flags

--- .github/tests/macos.sh
+++ .github/tests/macos.sh
@@ -12,11 +12,11 @@ errorOut () {
 
 bash_exe="/usr/local/bin/bash"
 
-if [[ $(bash fetch --config sample.config.conf -v) ]]; then
+if [[ $(bash fetch --config sample.config.conf --lib . -v) ]]; then
     successOut 'Fetched current output:'
-    ${bash_exe} fetch --config sample.config.conf -v
+    ${bash_exe} fetch --config sample.config.conf --lib . -v
 else
     errorOut 'fetch failed output:'
-    ${bash_exe} fetch --config sample.config.conf -v
+    ${bash_exe} fetch --config sample.config.conf --lib . -v
     exit 1
 fi
