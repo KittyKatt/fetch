078fb3358aff007ea5b63c207a26b7b898292bd4
Katie Bohnenkamper <kittykatt@kittykatt.co>
2022-04-30 12:37:14 -0500

    Update Fedora logo to be reflect current logo - from screenFetch issues

--- ascii/fedora.sh
+++ ascii/fedora.sh
@@ -8,22 +8,24 @@ if [ ! "${config_text[color]}" == "off" ]; then
 fi
 startline=0
 read -rd '' asciiLogo << 'EOF'
-${c2}           /:-------------:\
-${c2}        :-------------------::
-${c2}      :-----------${c1}/shhOHbmp${c2}---:\
-${c2}    /-----------${c1}omMMMNNNMMD  ${c2}---:
-${c2}   :-----------${c1}sMMMMNMNMP${c2}.    ---:
-${c2}  :-----------${c1}:MMMdP${c2}-------    ---\
-${c2} ,------------${c1}:MMMd${c2}--------    ---:
-${c2} :------------${c1}:MMMd${c2}-------    .---:
-${c2} :----    ${c1}oNMMMMMMMMMNho${c2}     .----:
-${c2} :--     .${c1}+shhhMMMmhhy++${c2}   .------/
-${c2} :-    -------${c1}:MMMd${c2}--------------:
-${c2} :-   --------${c1}/MMMd${c2}-------------;
-${c2} :-    ------${c1}/hMMMy${c2}------------:
-${c2} :--${c1} :dMNdhhdNMMNo${c2}------------;
-${c2} :---${c1}:sdNMMMMNds:${c2}------------:
-${c2} :------${c1}:://:${c2}-------------::
-${c2} :---------------------://
+${c2}             .',;::::;,'.
+${c2}         .';:cccccccccccc:;,.
+${c2}      .;cccccccccccccccccccccc;.
+${c2}    .:cccccccccccccccccccccccccc:.
+${c2}  .;ccccccccccccc;${c1}.:dddl:.${c2};ccccccc;.
+${c2} .:ccccccccccccc;${c1}OWMKOOXMWd${c2};ccccccc:.
+${c2}.:ccccccccccccc;${c1}KMMc${c2};cc;${c1}xMMc${c2}:ccccccc:.
+${c2},cccccccccccccc;${c1}MMM.${c2};cc;${c1};WW:${c2}:cccccccc,
+${c2}:cccccccccccccc;${c1}MMM.${c2};cccccccccccccccc:
+${c2}:ccccccc;${c1}oxOOOo${c2};${c1}MMM0OOk.${c2};cccccccccccc:
+${c2}cccccc:${c1}0MMKxdd:${c2};${c1}MMMkddc.${c2};cccccccccccc;
+${c2}ccccc:${c1}XM0'${c2};cccc;${c1}MMM.${c2};cccccccccccccccc'
+${c2}ccccc;${c1}MMo${c2};ccccc;${c1}MMW.${c2};ccccccccccccccc;
+${c2}ccccc;${c1}0MNc.${c2}ccc${c1}.xMMd${c2}:ccccccccccccccc;
+${c2}cccccc;${c1}dNMWXXXWM0:${c2}:cccccccccccccc:,
+${c2}cccccccc;${c1}.:odl:.${c2};cccccccccccccc:,.
+${c2}:cccccccccccccccccccccccccccc:'.
+${c2}.:cccccccccccccccccccccc:;,..
+${c2}  '::cccccccccccccc::;,.
 ${c2}
 EOF
