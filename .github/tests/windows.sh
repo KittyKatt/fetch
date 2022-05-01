4e432e1da21cadd459841b0e22416c327ee35120
Katie Bohnenkamper <kittykatt@kittykatt.co>
2022-04-30 21:35:19 -0500

    Trunk linting and formatting for workflows and tests

--- .github/tests/windows.sh
+++ .github/tests/windows.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 reset=$'\e[0m'
-successOut () {
-    success=$'\033[0m\033[32m'
-    printf '%b\n' ">> ${success}${1}${reset}"
+successOut()  {
+  success=$'\033[0m\033[32m'
+  printf '%b\n' ">> ${success}${1}${reset}"
 }
-errorOut () {
-    error=$'\033[0m\033[1;31m'
-    printf '%b\n' "${error}${1}${reset}"
+errorOut()  {
+  error=$'\033[0m\033[1;31m'
+  printf '%b\n' "${error}${1}${reset}"
 }
 
-if [[ $(bash fetch --config sample.config.conf --lib . -v) ]]; then
-    successOut 'Fetched current output:'
-    bash fetch --config sample.config.conf --lib . -v
+# shellcheck disable=SC2312
+if [[ -n $(bash fetch --config sample.config.conf --lib . -v) ]]; then
+  successOut 'Fetched current output:'
+  bash fetch --config sample.config.conf --lib . -v
 else
-    errorOut 'fetch failed output:'
-    bash fetch --config sample.config.conf --lib . -v
-    exit 1
+  errorOut 'fetch failed output:'
+  bash fetch --config sample.config.conf --lib . -v
+  exit 1
 fi
